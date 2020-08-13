638c1cf363602ce8a956ca0d85b49c563b988f21
Joo Hyun Jeon <joohyun.jeon@mail.utoronto.ca>
2020-08-13 19:39:03 -0400

    quick fix bug

--- client/src/actions/goalActions.js
+++ client/src/actions/goalActions.js
@@ -63,83 +63,6 @@ export const getGoals = (goalList) => {
         });
 };
 
-export async function findID2(app) {
-
-    const currUser = app.state.currentUser;
-    const currUsername = currUser.username;
-
-    const res = await fetch('/users');
-    const json = await res.json();
-
-    const userList = json.users;
-    userList.filter((user) => {
-        if (user.username === currUsername){
-            // get goals
-            //userID = user._id;
-            const userID = JSON.stringify(user._id);
-            //console.log(user);
-            return JSON.stringify(user._id);
-        }
-    });
-
-}
-
-
-//export const setOwner = (app, formValues) => {
-export const findID = (app) => {
-    const currUser = app.state.currentUser;
-    const currUsername = currUser.username;
-    //const currUserID = currUser.id;
-    //console.log(currUserID);
- 
-    // find user
-    let userID = '';
-
-    fetch("/users")
-        .then(res => {
-            if (res.status === 200) {
-                return res.json();
-            } else {
-                alert("Could not get users");
-            }
-        })
-        .then(json => { //json===users
-                // find correct user
-                const userList = json.users;
-                userList.filter((user) => {
-                    if (user.username === currUsername){
-                        // get goals
-                        //userID = user._id;
-                        userID = JSON.stringify(user._id);
-                        console.log("user:");
-                        console.log(JSON.stringify(user));
-                        console.log("user._id:");
-                        console.log(JSON.stringify(user._id));
-                        //goalForm.setState({ currUserID : userID });
-                    }
-                });
-            //console.log("userID: "+ userID); //works
-            //return userID; // if I log this its correct but cannot retreive it as string
-        })
-        .catch(error => {
-            console.log(error);
-        });
-
-    return userID;
-}
-
-
-export const returnURL = (app) => {
-    const url = '';
-    findID(app).then((id) => {
-        console.log("return URL"+id);
-        console.log(id);
-        url.concat(id.toString());
-        console.log("returnURL url: "+url);
-    })
-    return url;
-}
-
 export const setOwner = (app, goalForm) => {
 // 5f2b2e87e920607eb31129c4 -> user1
     // get currentUsername
@@ -156,7 +79,7 @@ export const setOwner = (app, goalForm) => {
     //create link to user w/ id
     const link = '/users/';
     const url = link.concat(currUsername);
-    
+
     // make new patch request
     const request = new Request(url, {
         method: 'PATCH',
@@ -186,13 +109,5 @@ export const setOwner = (app, goalForm) => {
 export async function updateGoals(goalList, goalForm, app) {
     addGoalJSON(goalForm);
     getGoals(goalList);
-
-    // const id = findID(app);
-    // console.log("updateGoals id: ");
-    // console.log(id);
-
-    // const result = await findID2(app);
-    // console.log("findID2 result: ", result);
-
     setOwner(app, goalForm);
 }
